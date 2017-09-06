558dae65b4eb81aaafc050beb49c1c5ce6a6b569
Nikita Aristov <nikita.aristov@gmail.com>
2017-09-06 14:13:49 +0300

    compile module with rollup

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const external = ['prax']
+
+module.exports = [
+  {
+    input: 'lib/scripts/index.js',
+    output: {file: 'es/purelab-ui.js', format: 'es'},
+    external,
+    plugins: [
+      require('rollup-plugin-babel')({exclude: 'node_modules/**'}),
+    ],
+  },
+  {
+    input: 'es/purelab-ui.js',
+    output: {file: 'dist/purelab-ui.js', format: 'cjs'},
+    external,
+  },
+  // For evaluating minified size
+  {
+    input: 'dist/purelab-ui.js',
+    output: {file: 'dist/purelab-ui.min.js', format: 'cjs'},
+    external,
+    plugins: [
+      require('rollup-plugin-uglify')({
+        mangle: true,
+        toplevel: true,
+        compress: true,
+      }),
+    ],
+  },
+]
